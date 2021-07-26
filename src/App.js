56519b28b99b41efbfe04f4adb07b54d74749ee0
alllekseev <01.wftp@gmail.com>
2021-07-26 19:55:35 +0300

    Lesson1: is ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './Components/Message/Message.js';
 
-function App() {
+const someMessage = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus ad at beatae blanditiis consequuntur hic laboriosam laborum minima optio quaerat voluptas voluptates, voluptatum! Accusamus asperiores eius laborum maxime mollitia nulla, provident quo ratione repudiandae ullam? Consequatur dicta ex, facilis fuga fugit harum minima mollitia neque provident, quia rerum, tempora?\n';
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <header className="App-header">
+              My First React App
+              <h3>Hello, {props.name}!</h3>
+          </header>
+          <Message message={someMessage}/>
+      </div>
   );
 }
 
